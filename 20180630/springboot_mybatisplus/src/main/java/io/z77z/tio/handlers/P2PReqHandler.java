1f6737d104657fc5a9c7251ac9dc21a05f4d7610
liupingan <ray.pa.liu@foxconn.com>
2018-07-07 17:49:37 +0800

    测试

---
+++ 20180630/springboot_mybatisplus/src/main/java/io/z77z/tio/handlers/P2PReqHandler.java
@@ -0,0 +1,30 @@
+package io.z77z.tio.handlers;
+
+import java.nio.ByteBuffer;
+import java.util.Date;
+
+import org.tio.core.Aio;
+import org.tio.core.ChannelContext;
+import org.tio.json.Json;
+
+import io.z77z.tio.body.P2PReqBody;
+import io.z77z.tio.server.BarrageHandlerIntf;
+import io.z77z.tio.server.BarragePacket;
+import io.z77z.tio.server.BarrageSessionContext;
+import io.z77z.tio.server.Type;
+
+public class P2PReqHandler implements BarrageHandlerIntf<P2PReqBody>{
+
+	@Override
+	public Object handler(
+			BarragePacket packet,
+			String jsonStr,
+			ChannelContext<BarrageSessionContext, BarragePacket, Object> channelContext)
+			throws Exception {
+		System.out.println(jsonStr);
+		BarragePacket barragePacket = new BarragePacket((new Date().toString()+jsonStr).getBytes(BarragePacket.CHARSET));
+		Aio.send(channelContext, barragePacket);
+		return null;
+	}
+
+}
